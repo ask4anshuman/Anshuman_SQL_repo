69332f934ab0a386acccc4d67ca29369af258a34
Anshuman Kumar <ask4anshuman@gmail.com>
2026-04-18 20:12:18 +0100

    new test

--- staging/sample_sales_summary.sql
+++ staging/sample_sales_summary.sql
@@ -47,7 +47,7 @@ GROUP BY
     s.store_name,
     ms.sales_month,
     cs.segment_name
-HAVING SUM(ms.total_amount) >= 60
+HAVING SUM(ms.total_amount) >= 600000
 ORDER BY
     ms.sales_month,
     s.store_name,
