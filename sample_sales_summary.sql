6a97ad91999ab31d24d6d32d7d67cfd1c3d77421
Anshuman Kumar <ask4anshuman@gmail.com>
2026-04-18 12:02:23 +0100

    test

--- sample_sales_summary.sql
+++ sample_sales_summary.sql
@@ -47,7 +47,7 @@ GROUP BY
     s.store_name,
     ms.sales_month,
     cs.segment_name
-HAVING SUM(ms.total_amount) >= 100000
+HAVING SUM(ms.total_amount) >= 500
 ORDER BY
     ms.sales_month,
     s.store_name,
